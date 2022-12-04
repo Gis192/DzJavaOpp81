aa17d7b429b7eba0e5305f3ac928e7b5c34b01b3
Ivan Golikov <gis92@rambler.ru>
2022-12-04 16:29:49 +0300

    HZ

---
+++ src/main/java/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.patronymic = "Иванович";
+        post.surname = "Иванов";
+        post.passport = "4444 № 444444";
+        post.phone = "+7 (999) 999-99-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+
+
+
+        System.out.println(post.name);
+        System.out.println(post.patronymic);
+        System.out.println(post.surname);
+        System.out.println( post.passport);
+        System.out.println(post.phone);
+        System.out.println(post.subscription);
+    }
+}
